2fcfb1a2c5e7138b8947645d001bc1adf5bcc939
John Ky <newhoggy@gmail.com>
2018-06-28 23:04:35 +1000

    Count number of characters

---
+++ csv/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 512
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    printf("Insufficient arguments\n");
+    return 1;
+  }
+
+  char *filename = argv[1];
+
+  FILE *file = fopen(filename, "r");
+
+  if (!file) {
+    printf("Error opening file\n");
+    return 1;
+  }
+
+  char buffer[512];
+
+  int total_bytes_read = 0;
+
+  int bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
+
+  while (bytes_read > 0) {
+    total_bytes_read += bytes_read;
+
+    bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
+  }
+
+  printf("Total bytes read %d\n", total_bytes_read);
+
+
+  return 0;
+}
