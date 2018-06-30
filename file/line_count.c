13397d2b896c7c9efc29fab6dbd6b2603ff71c79
John Ky <newhoggy@gmail.com>
2018-06-30 23:27:05 +1000

    Stuff

--- file/line_count.c
+++ file/line_count.c
@@ -10,21 +10,36 @@
 // __m256i _mm256_broadcastb_epi8 (__m128i a)
 // __m256i _mm256_cmpeq_epi64 (__m256i a, __m256i b)
 
-void process_data(char *text, size_t bytes_read, uint64_t *bits)
+void process_data(char *text, size_t bytes_read, uint64_t *bits_w64, __m256i ws_newlines)
 {
-  // // __declspec(align(8)) const float num = 20;
-  // const __m128i x = _mm_set_epi32(1,2,3,4);
+  char *bytes_end = text + bytes_read;
+  uint32_t *bits_w32 = (uint32_t *)bits_w64;
 
-  // __m128i mask = 1; // (__m128i)(int)',';
+  for (size_t i = 0; i < bytes_read; i += 32) {
+    size_t bytes_read_iter = bytes_read - i;
 
+    if (bytes_read_iter > 32) {
+      bytes_read_iter = 32;
+    }
+
+    __m256i matches_bytes = _mm256_cmpeq_epi8(*(__m256i*)(text + i), ws_newlines);
+
+    int matches_bits = _mm256_movemask_epi8(matches_bytes);
+
+    *(bits_w32 + i / 32) = (0xffffffff >> (32 - bytes_read_iter)) & (uint32_t)matches_bits;
+  }
+}
+
+__m256i broadcast_u8(uint8_t w)
+{
+  __m128i w128 = {w};
+
+  return _mm256_broadcastb_epi8(w128);
 }
 
 int main(int argc, char **argv)
 {
   if (argc < 3) {
-    printf("Error: Insufficient arguments\n");
-    printf("\n");
-    printf("%s <src-file> <target-file>\n", argv[0]);
     return 1;
   }
 
@@ -35,12 +50,13 @@ int main(int argc, char **argv)
   FILE *out_file  = fopen(out_filename, "w");
 
   if (!in_file) {
-    printf("Error opening file\n");
     return 1;
   }
 
   char buffer[BUFFER_SIZE];
   uint64_t bits[BUFFER_SIZE];
+
+  __m256i ws_newline = broadcast_u8((uint8_t)'\n');
 
   memset(bits, 0, BUFFER_SIZE);
 
@@ -51,12 +67,14 @@ int main(int argc, char **argv)
   while (bytes_read > 0) {
     total_bytes_read += bytes_read;
 
-    process_data(buffer, bytes_read, bits);
+    process_data(buffer, bytes_read, bits, ws_newline);
 
     fwrite((char*)bits, 1, (bytes_read + 7) / 8, out_file);
 
     bytes_read = fread(buffer, 1, BUFFER_SIZE, in_file);
   }
+
+  fflush(out_file);
 
   fclose(in_file);
   fclose(out_file);
