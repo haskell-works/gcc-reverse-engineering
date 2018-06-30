d8f29dafc8d1dea85cef1c195863707630590548
John Ky <newhoggy@gmail.com>
2018-06-30 23:58:24 +1000

    Optimisations

--- file/line_count.c
+++ file/line_count.c
@@ -5,7 +5,7 @@
 #include <immintrin.h>
 #include <string.h>
 
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 8192
 
 // __m256i _mm256_broadcastb_epi8 (__m128i a)
 // __m256i _mm256_cmpeq_epi64 (__m256i a, __m256i b)
@@ -15,18 +15,28 @@ void process_data(char *text, size_t bytes_read, uint64_t *bits_w64, __m256i ws_
   char *bytes_end = text + bytes_read;
   uint32_t *bits_w32 = (uint32_t *)bits_w64;
 
-  for (size_t i = 0; i < bytes_read; i += 32) {
-    size_t bytes_read_iter = bytes_read - i;
+  if ((bytes_read & 0x1f) == 0) {
+    for (; text != bytes_end; text += 32) {
+      __m256i matches_bytes = _mm256_cmpeq_epi8(*(__m256i*)text, ws_newlines);
 
-    if (bytes_read_iter > 32) {
-      bytes_read_iter = 32;
+      int matches_bits = _mm256_movemask_epi8(matches_bytes);
+
+      *(bits_w32++) = (uint32_t)matches_bits;
     }
+  } else {
+    for (size_t i = 0; i < bytes_read; i += 32) {
+      size_t bytes_read_iter = bytes_read - i;
 
-    __m256i matches_bytes = _mm256_cmpeq_epi8(*(__m256i*)(text + i), ws_newlines);
+      if (bytes_read_iter > 32) {
+        bytes_read_iter = 32;
+      }
 
-    int matches_bits = _mm256_movemask_epi8(matches_bytes);
+      __m256i matches_bytes = _mm256_cmpeq_epi8(*(__m256i*)(text + i), ws_newlines);
 
-    *(bits_w32 + i / 32) = (0xffffffff >> (32 - bytes_read_iter)) & (uint32_t)matches_bits;
+      int matches_bits = _mm256_movemask_epi8(matches_bytes);
+
+      *(bits_w32 + i / 32) = (0xffffffff >> (32 - bytes_read_iter)) & (uint32_t)matches_bits;
+    }
   }
 }
 
@@ -54,11 +64,11 @@ int main(int argc, char **argv)
   }
 
   char buffer[BUFFER_SIZE];
-  uint64_t bits[BUFFER_SIZE];
+  uint64_t bits[BUFFER_SIZE / 64];
 
   __m256i ws_newline = broadcast_u8((uint8_t)'\n');
 
-  memset(bits, 0, BUFFER_SIZE);
+  memset(bits, 0, BUFFER_SIZE / 64);
 
   size_t total_bytes_read = 0;
 
